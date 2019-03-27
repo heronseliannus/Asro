399cdbba5bb7c982fbbc096ed1d45b8e3cbd54a5
heronselianus <herselianus1795@gmail.com>
2019-03-27 23:53:29 +0700

    pop up & main

--- resources/views/asro-kalbar.blade.php
+++ resources/views/asro-kalbar.blade.php
@@ -52,7 +52,7 @@
                     <span class="icon-bar"></span>
                 </button>
                 <a href="#" class="navbar-brand logo">
-                    <img src="images/logo asro.png" width="250" height="10" alt="">
+                    <img src="images/logo asro.png" width="200" height="10" alt="">
                 </a>
             </div>
             <!--Logo/-->
