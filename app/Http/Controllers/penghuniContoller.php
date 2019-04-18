799aded1469adb25fd8b2cecefb3437642227404
heronseliannus <heronselianus1795@gmail.com>
2019-04-18 14:25:03 +0700

    controller formulir

---
+++ app/Http/Controllers/penghuniContoller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class penghuniContoller extends Controller
+{
+    public function view ()
+    {
+        return view ('formulir/lohong');
+    }
+}
