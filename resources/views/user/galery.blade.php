a5c9d61617a84ef215282ea9ee7caab752b03d9d
heronselianus <herselianus1795@gmail.com>
2019-04-24 12:41:57 +0700

    galery

--- resources/views/user/galery.blade.php
+++ resources/views/user/galery.blade.php
@@ -1,4 +1,126 @@
 @extends('layout.menu')
-@section('coontent')
+@section('title','galery asro 2017')
+@section('content')
+
+<section class="section-padding gray-bg" id="blog-page">
+    <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-3">
+                    <div class="single-blog">
+                        <div class="blog-photo">
+                            <img src="images/asro2.jpg" alt="">
+                        </div>
+                        <div class="blog-content">
+                            <h3><a href="#">Kegiatan Tahunan Telkom CorpU</a></h3>
+                            <ul class="blog-meta">
+                                <li><span class="ti-user"></span> <a href="#">Admin</a></li>
+                                <li><span class="ti-calendar"></span> <a href="#">Oktober 10, 2018</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-3">
+                    <div class="single-blog">
+                        <div class="blog-photo">
+                            <img src="images/asro2.jpg" alt="">
+                        </div>
+                        <div class="blog-content">
+                            <h3><a href="#">Kegiatan Tahunan Telkom CorpU</a></h3>
+                            <ul class="blog-meta">
+                                <li><span class="ti-user"></span> <a href="#">Admin</a></li>
+                                <li><span class="ti-calendar"></span> <a href="#">Oktober 10, 2018</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-3">
+                    <div class="single-blog">
+                        <div class="blog-photo">
+                            <img src="images/asro2.jpg" alt="">
+                        </div>
+                        <div class="blog-content">
+                            <h3><a href="#">Kegiatan Tahunan Telkom CorpU</a></h3>
+                            <ul class="blog-meta">
+                                <li><span class="ti-user"></span> <a href="#">Admin</a></li>
+                                <li><span class="ti-calendar"></span> <a href="#">Oktober 10, 2018</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-3">
+                    <div class="single-blog">
+                        <div class="blog-photo">
+                            <img src="images/asro2.jpg" alt="">
+                        </div>
+                        <div class="blog-content">
+                            <h3><a href="#">Kegiatan Tahunan Telkom CorpU</a></h3>
+                            <ul class="blog-meta">
+                                <li><span class="ti-user"></span> <a href="#">Admin</a></li>
+                                <li><span class="ti-calendar"></span> <a href="#">Oktober 10, 2018</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-3">
+                    <div class="single-blog">
+                        <div class="blog-photo">
+                            <img src="images/asro2.jpg" alt="">
+                        </div>
+                        <div class="blog-content">
+                            <h3><a href="#">Kegiatan Tahunan Telkom CorpU</a></h3>
+                            <ul class="blog-meta">
+                                <li><span class="ti-user"></span> <a href="#">Admin</a></li>
+                                <li><span class="ti-calendar"></span> <a href="#">Oktober 10, 2018</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-3">
+                    <div class="single-blog">
+                        <div class="blog-photo">
+                            <img src="images/asro2.jpg" alt="">
+                        </div>
+                        <div class="blog-content">
+                            <h3><a href="#">Kegiatan Tahunan Telkom CorpU</a></h3>
+                            <ul class="blog-meta">
+                                <li><span class="ti-user"></span> <a href="#">Admin</a></li>
+                                <li><span class="ti-calendar"></span> <a href="#">Oktober 10, 2018</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-3">
+                    <div class="single-blog">
+                        <div class="blog-photo">
+                            <img src="images/asro2.jpg" alt="">
+                        </div>
+                        <div class="blog-content">
+                            <h3><a href="#">Kegiatan Tahunan Telkom CorpU</a></h3>
+                            <ul class="blog-meta">
+                                <li><span class="ti-user"></span> <a href="#">Admin</a></li>
+                                <li><span class="ti-calendar"></span> <a href="#">Oktober 10, 2018</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-3">
+                    <div class="single-blog">
+                        <div class="blog-photo">
+                            <img src="images/asro2.jpg" alt="">
+                        </div>
+                        <div class="blog-content">
+                            <h3><a href="#">Kegiatan Tahunan Telkom CorpU</a></h3>
+                            <ul class="blog-meta">
+                                <li><span class="ti-user"></span> <a href="#">Admin</a></li>
+                                <li><span class="ti-calendar"></span> <a href="#">Oktober 10, 2018</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+    </section>
+    </footer>
+
 
 @endsection
