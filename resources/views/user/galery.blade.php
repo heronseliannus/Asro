a49bca997c0dfbc5dc4fe9428b8950cb36f879b6
heronselianus <herselianus1795@gmail.com>
2019-05-01 19:38:06 +0700

    tambahan penghuni

--- resources/views/user/galery.blade.php
+++ resources/views/user/galery.blade.php
@@ -411,6 +411,20 @@
                         </div>
                     </div>
                 </div>
+                <div class="col-xs-12 col-sm-3">
+                    <div class="single-blog">
+                        <div class="blog-photo">
+                            <img src="images/Penghuni/Juna.jpg" alt="">
+                        </div>
+                        <div class="blog-content">
+                            <h3><a href="#">Maraga Satrio Arjuna</a></h3>
+                            <ul class="blog-meta">
+                                <li><span class="ti-user"></span> <a href="#">Admin</a></li>
+                                <li><span class="ti-calendar"></span> <a href="#">Oktober 10, 2018</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
     </div>
 </section>
