dd9706b82d170b3cb7d7da99bca837be4e8eff64
heronselianus <herselianus1795@gmail.com>
2019-05-01 19:31:19 +0700

    tambah galery

--- resources/views/user/galery.blade.php
+++ resources/views/user/galery.blade.php
@@ -397,6 +397,20 @@
                         </div>
                     </div>
                 </div>
+                <div class="col-xs-12 col-sm-3">
+                    <div class="single-blog">
+                        <div class="blog-photo">
+                            <img src="images/Penghuni/Igo.jpg" alt="">
+                        </div>
+                        <div class="blog-content">
+                            <h3><a href="#">Wilibrodus Igo</a></h3>
+                            <ul class="blog-meta">
+                                <li><span class="ti-user"></span> <a href="#">Admin</a></li>
+                                <li><span class="ti-calendar"></span> <a href="#">Oktober 10, 2018</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
     </div>
     </section>
