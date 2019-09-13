31a1beb2ea46822fc43fed80e03b400bc82bc16f
heronselianus <herselianus1795@gmail.com>
2019-09-13 23:48:02 +0700

    kepengurusan

--- resources/views/pengurus/pengurus2016.blade.php
+++ resources/views/pengurus/pengurus2016.blade.php
@@ -5,42 +5,14 @@
     
     <!--Feature-area-->
     <section class="gray-bg section-padding" id="service-page">
-        <div class="container">
             <div class="row">
-                <div class="col-xs-12 col-sm-4">
+                <div class="col-xs-12 col-sm-12">
                     <div class="box">
-                        <div class="box-icon">
-                            <img src="images/Kepengurusan/2016-2017/Aris.jpg" alt="">
-                        </div>
                         <h4>Ketua Asrama Tahun Kepengurusan 2016-2017</h4>
-                    </div>
-                </div>
-                <div class="col-xs-12 col-sm-4">
-                    <div class="box">
-                        <div class="box-icon">
-                            <img src="images/Kepengurusan/2016-2017/Dani.jpg" alt="">
-                        </div>
-                        <h4>Seketaris Asrama Tahun Kepengurusan 2016-2017</h4>
-                    </div>
-                </div>
-                <div class="col-xs-12 col-sm-4">
-                    <div class="box">
-                        <div class="box-icon">
-                            <img src="images/Kepengurusan/2016-2017/Ipung.jpg" alt="">
-                        </div>
-                        <a href="#"><h4>Bendahara Asrama Tahun Kepengurusan 2016-2017</h4></a>
-                    </div>
-                </div>
-                <div class="col-xs-12 col-sm-4">
-                    <div class="box">
-                        <div class="box-icon">
-                            <img src="images/Kepengurusan/2016-2017/Juna.jpg" alt="">
-                        </div>
-                        <a href="#"><h4>Kebersihan Ketua Asrama Tahun Kepengurusan 2016-2017</h4></a>
+                            <img src="images/Kepengurusan/2016-2017/bpa16.jpg" alt="">                     
                     </div>
                 </div>
             </div>
-        </div>
     </section>
     <!--Feature-area/-->
 
