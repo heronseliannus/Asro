186af765ac7863ed110001dc1a935c61038446f1
heronseliannus <heronselianus1795@gmail.com>
2019-09-13 21:00:56 +0700

    changes new view content

--- resources/views/pengurus/pengurus2017.blade.php
+++ resources/views/pengurus/pengurus2017.blade.php
@@ -7,7 +7,7 @@
     <section class="gray-bg section-padding" id="service-page">
         <div class="row">
             <div class="col-xs-12 col-sm-12">
-                <img src="images/Kepengurusan/2017-2018/bpa2017" alt="">
+                <img src="images/Kepengurusan/2017-2018/bpa2017.jpg" alt="">
             </div>
         </div>
     </section>
