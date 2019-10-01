92df6063a86adaccc3db4675922f32df40a91211
Heron <heronselianus1795@gmail.com>
2019-10-01 16:33:09 +0700

    login user

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -1,75 +1,6 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="author" content="John Doe">
-    <meta name="description" content="">
-    <meta name="keywords" content="HTML,CSS,XML,JavaScript">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <!-- Title -->
-    <title>Menu Utama </title>
-    <!-- Place favicon.ico in the root directory -->
-    <link rel="logo-asro.jpg" href="images/logo-asro.jpg">
-    <link rel="shortcut icon" type="image/logo.jpg" href="images/logo-asro.jpg" />
-    <!-- Plugin-CSS -->
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css')}}">
-    <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css')}}">
-    <link rel="stylesheet" href="{{ asset('css/themify-icons.css')}}">
-    <link rel="stylesheet" href="{{ asset('css/magnific-popup.css')}}">
-    <link rel="stylesheet" href="{{ asset('css/animate.css')}}">
-    <!-- Main-Stylesheets -->
-    <link rel="stylesheet" href="{{ asset('css/normalize.css')}}">
-    <link rel="stylesheet" href="{{ asset('css/style.css')}}">
-    <link rel="stylesheet" href="{{ asset('css/responsive.css')}}">
-    <script src="{{ asset('js/vendor/modernizr-2.8.3.min.js')}}"></script>
-
- 
-        <!-- <script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-        <script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script> -->
-
-</head>
-
-<body data-spy="scroll" data-target="#primary-menu">
-    <div class="preloader">
-        <div class="sk-folding-cube">
-            <div class="sk-cube1 sk-cube"></div>
-            <div class="sk-cube2 sk-cube"></div>
-            <div class="sk-cube4 sk-cube"></div>
-            <div class="sk-cube3 sk-cube"></div>
-        </div>
-    </div>
-    <!--Mainmenu-area-->
-    <div class="mainmenu-area" data-spy="affix" data-offset-top="100">
-        <div class="container">
-            <!--Logo-->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#primary-menu">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a href="asrobandung" class="navbar-brand logo">
-                    <img src="images/logo asro.png" width="200" height="10" alt="">
-                </a>
-            </div>
-            <!--Logo/-->
-            <nav class="collapse navbar-collapse" id="primary-menu">
-                <ul class="nav navbar-nav navbar-right">
-                    <li class="active"><a href="#home-page">Home</a></li>
-                    <li><a href="asrobandung">Info</a></li>
-                    <li><a href="asrobandung">Profil</a></li>
-                    <li><a href="asrobandung">Galery</a></li>
-                    <li><a href="asrobandung">Contact</a></li>
-                    <li><a href="asrobandung">Daftar Penghuni</a></li>
-                    <li><a href="formdaftar">Pendaftaran</a></li>
-                    <li><a href="login">Login</a></li>
-                </ul>
-            </nav>
-        </div>
-    </div>
+@extends('layout.menu')
+@section('title','silahkan input data anda')
+@section('content')
 
 <footer class="footer-area relative sky-bg" id="register">
         <div class="absolute footer-bg"></div>
@@ -156,5 +87,4 @@
             </div>
         </div>
 </footer>
-</body>
-</html>
+@endsection
