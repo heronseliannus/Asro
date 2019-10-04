728c5a3f205111914db00f036734498d3803eead
heronseliannus <heronselianus1795@gmail.com>
2019-10-04 18:11:56 +0700

    data models and controller

--- app/Http/Controllers/Datamhscontroller.php
+++ app/Http/Controllers/Datamhscontroller.php
@@ -8,13 +8,14 @@ use Illuminate\Support\facades\DB;
 
 class Datamhscontroller extends Controller
 {
-    public function ondext()
+    public function index()
     {
         return view('usermaster/datamhs');
     }
     public function show()
     {
-        $datamhss = DB::table('datamhs')->get($id);
+        $registers = DB::table('register')->get();
+        
 
         return view('usermaster/datamhs',['datamhs' => $datamhs]);
     }
