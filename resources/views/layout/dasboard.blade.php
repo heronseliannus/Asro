e2fd28c4464a9f4500cea356c6c55d69910be6f5
heronseliannus <heronselianus1795@gmail.com>
2019-10-11 02:13:37 +0700

    create dasboard

--- resources/views/layout/dasboard.blade.php
+++ resources/views/layout/dasboard.blade.php
@@ -3,7 +3,36 @@
 
 @section('content')
 
-
+<section class="gray-bg section-padding" id="service-page">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-4">
+                    <div class="box">
+                        <div class="box-icon">
+                            <img src="images/Makrab/Makrab 1.jpg" alt="">
+                        </div>
+                        <h4>Makrab KPMKB-Bandung 2019</h4>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-4">
+                    <div class="box">
+                        <div class="box-icon">
+                            <img src="images/Tari.jpg" alt="">
+                        </div>
+                        <h4>Seni Dan Kreasi KPMKB 2018</h4>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-4">
+                    <div class="box">
+                        <div class="box-icon">
+                            <img src="images/Angkatan/kemah 1.jpg" alt="">
+                        </div>
+                        <a href="#"><h4>Kegiatan Keakraban 2017</h4></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+</section>
 
 
 
