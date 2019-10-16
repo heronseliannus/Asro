a8ddac3eeb7ef6c902853290b316baaf49d112e0
heronseliannus <heronselianus1795@gmail.com>
2019-10-16 17:18:10 +0700

    delete data controller

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -28,16 +28,20 @@ class RegisterController extends Controller
         //     'jurusan'=>'Teknik Elektro'
         //     ]
         // ]);
+
         //update
         // DB::table('register')->where('namalengkap', 'insert10','13' )
         //                      ->update(['namalengkap'=>'insert15']);
 
+        //delete
+        // DB::table('register')->where('namalengkap', 'delete2')->delete();
         
         $registers = DB::table('register')->get();
      
         return view('usermaster/datamhs',['registers' => $registers]);
         //insert data manually
         //update data manually where namalengkap
+        //delete data where usernamen/namalengkap
         //get data register input on database mysql    
         //redirect view on datamhs
     }
