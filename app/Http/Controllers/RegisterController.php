d42260d96c074654219a64f476400b5c737d7329
heronseliannus <heronselianus1795@gmail.com>
2019-10-16 16:59:10 +0700

    update data registercontroller

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -15,22 +15,29 @@ class RegisterController extends Controller
     }
     public function show()
     {
-        DB::table('register')->insert([
-            ['namalengkap'=>'insert',
-            'alamat'  =>'Bandung, Indonesia',
-            'jenis_kelamin' => 'Perempuan',
-            'email'  => 'isnert1@gmail.com',
-            're_email' => 'isert1@gamil.com',
-            'kabupaten' => 'Kab. Bandung',
-            'kota' =>'Bandung',
-            'perguruan_tinggi' =>'PPG Bandung',
-            'jurusan'=>'Teknik Elektro'
-            ]
-        ]);
+        //insert
+        // DB::table('register')->insert([
+        //     ['namalengkap'=>'insert',
+        //     'alamat'  =>'Bandung, Indonesia',
+        //     'jenis_kelamin' => 'Perempuan',
+        //     'email'  => 'isnert1@gmail.com',
+        //     're_email' => 'isert1@gamil.com',
+        //     'kabupaten' => 'Kab. Bandung',
+        //     'kota' =>'Bandung',
+        //     'perguruan_tinggi' =>'PPG Bandung',
+        //     'jurusan'=>'Teknik Elektro'
+        //     ]
+        // ]);
+        //update
+        // DB::table('register')->where('namalengkap', 'insert10','13' )
+        //                      ->update(['namalengkap'=>'insert15']);
+
+        
         $registers = DB::table('register')->get();
      
         return view('usermaster/datamhs',['registers' => $registers]);
         //insert data manually
+        //update data manually where namalengkap
         //get data register input on database mysql    
         //redirect view on datamhs
     }
